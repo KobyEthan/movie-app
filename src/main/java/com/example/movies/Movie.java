ff86cf662f51806e598f174945bbfef65beb7afc
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-06-19 21:31:58 -0500

    Database configuration and added the Movie class.

---
+++ src/main/java/com/example/movies/Movie.java
@@ -0,0 +1,30 @@
+package com.example.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Document(collection = "movies")
+// -------- lombok annotations --------
+// Takes care of getters, setters, and toString methods
+@Data
+// Makes a constructor with all the parameters
+@AllArgsConstructor
+// Makes a constructor wih no parameters
+@NoArgsConstructor
+// -------- lombok annotations --------
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+}
