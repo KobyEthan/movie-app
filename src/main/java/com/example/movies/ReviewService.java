d4d7e9caf7af1104e06fdde98d126e6f8f391113
Koby Smith-McCallister <kemccallister2002@gmail.com>
2024-06-22 00:09:26 -0500

    Finished up the reviews API

--- src/main/java/com/example/movies/ReviewService.java
+++ src/main/java/com/example/movies/ReviewService.java
@@ -2,6 +2,8 @@ package com.example.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +15,14 @@ public class ReviewService {
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId){
-        Review review = new Review(reviewBody);
+        Review review = reviewRepository.insert(new Review(reviewBody));
         reviewRepository.insert(review);
 
         mongoTemplate.update(Movie.class)
-                .matching(Criteria.where(""))
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
     }
 }
